89115a78e2aa5bbc8e2d901b3faa685fcec491d2
sriramadas balaji <balajisriramdas257@gmail.com>
2016-03-10 16:12:56 +0000

    source code

---
+++ Asp.netwebapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Asp.netwebapp.MvcApplication" Language="C#" %>
---
+++ Asp.netwebapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Asp.netwebapp.Startup))]
+namespace Asp.netwebapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
